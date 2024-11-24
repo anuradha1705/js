2770e71d48696ad9755e64c1b6efb8f7efe2fb23
anuradha1705 <anuradhakumawat2005@gmail.com>
2024-11-24 14:38:44 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+//let score =33
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber );
+//console.log(valueInNumber);
+
+
+
+//"33" => 33
+// "33abc" => NaN(NOT A NUMBER)
+// true => 1;  false => 0
+
+//let isLoggedIn = 1      //  1    => true , 0 => false
+let isLoggedIn = ""       // ""    => false
+//let isLoggedIn = "anu"  // "anu" =>true
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
