c00ffa428cc2454a60bea884be85f205d9d21d5e
anuradha1705 <anuradhakumawat2005@gmail.com>
2024-11-24 13:42:04 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs(alert),note beowser
+
+console.log(3+3); //code readability should be high
+
+
+console.log("anurdha");
+
+
+let name = "anu" //string datatype
+let age = 18
+let isloggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+//string => ""
+//boolean => true/ false
+//null => standalone value
+//undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof undefined); //undefined=undefined
+console.log(typeof null);// null=object
